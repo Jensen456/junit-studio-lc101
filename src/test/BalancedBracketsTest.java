a22c55b6cde8a87fff1bf9da994e03f79ef42b0b
Jensen456 <62215937+Jensen456@users.noreply.github.com>
2020-07-02 20:52:06 -0400

    Added Ch. 6 Studio, Unit Testing, BalancedBracketsTest

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -12,5 +12,39 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void initTest() {
+        assertEquals(true, main.BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertEquals(true, main.BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void bracketsAndStringReturnsTrue() {
+        assertEquals(true, main.BalancedBrackets.hasBalancedBrackets("[String]"));
+    }
+
+    @Test
+    public void nestedReturnsTrue() {
+        assertEquals(true, main.BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void onlyLeftBracketsReturnsFalse() {
+        assertEquals(false, main.BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    //@Test
+    //public void wrongOrderBracketsReturnsFalse() {
+    //    assertEquals(false, main.BalancedBrackets.hasBalancedBrackets("]["));
+    //}
+
+    @Test
+    public void otherMismatchReturnsFalse() {
+        assertEquals(false, main.BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
 
 }
